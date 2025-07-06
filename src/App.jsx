097a21f86cfbc28f32444bef939fae5074120173
Бобур <bosbacl713@gmail.com>
2025-07-06 22:06:32 +0500

    pages papkamni tug'irlab oldim

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-
+import { RouterProvider, createBrowserRouter } from "react-router"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import {
+  About, Checkout, Cart, Error, HomeLayout, Landing, Login, Orders, Products, Register, SingleProduct
+} from './pages'
 function App() {
 
   return (
