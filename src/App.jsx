43e7f3954ea83aa0ace273d7677d3ec66443ff16
Бобур <bosbacl713@gmail.com>
2025-07-06 22:32:15 +0500

    uzur xato ni tug'ilab oldim

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { RouterProvider, createBrowserRouter } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import {
+  About,
+  Checkout,
+  Cart,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct
+} from './pages'
 
 import { ErrorElement } from './components'
 // loaders
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -8,6 +9,6 @@ import { Toaster } from 'sonner'
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-    <Toaster position='bottom-right' />
+    <Toaster position='top-right' />
   </Provider>,
 )
